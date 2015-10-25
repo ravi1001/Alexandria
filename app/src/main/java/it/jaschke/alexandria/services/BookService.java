b7907dd4882684d49ba20fadbb95364b8f17b0b9
Ravi <ravi10010101@gmail.com>
2015-10-25 17:32:38 +0530

    fix: Fix crash while searching for book without internet connection

--- app/src/main/java/it/jaschke/alexandria/services/BookService.java
+++ app/src/main/java/it/jaschke/alexandria/services/BookService.java
@@ -2,8 +2,11 @@ package it.jaschke.alexandria.services;
 
 import android.app.IntentService;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -99,6 +102,18 @@ public class BookService extends IntentService {
         }
 
         bookEntry.close();
+
+        /* Book is not already present in the database, fetch from remote server. */
+
+        // Check if internet connection is available.
+        if(!isNetworkAvailable()) {
+            // No network, send local broadcast to main activity specifying the error message and return.
+            Intent messageIntent = new Intent(MainActivity.MESSAGE_EVENT);
+            messageIntent.putExtra(MainActivity.MESSAGE_KEY, getResources().getString(R.string.no_network));
+            LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(messageIntent);
+
+            return;
+        }
 
         HttpURLConnection urlConnection = null;
         BufferedReader reader = null;
@@ -222,5 +237,13 @@ public class BookService extends IntentService {
             getContentResolver().insert(AlexandriaContract.CategoryEntry.CONTENT_URI, values);
             values= new ContentValues();
         }
+    }
+
+    // Checks and returns whether network is available.
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
  }
